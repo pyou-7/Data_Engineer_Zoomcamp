e557e3ecfcc49c902c1ae7d06c8d2a5854fe1ba5
Patrick You <78947453+pyou-7@users.noreply.github.com>
2026-01-30 09:08:35 +0000

    chore: update files and .gitignore
    Apply psycopg2 driver, add docker-compose and ignore pipeline volumes

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from data-engineer-zoomcamp!")
-
-
-if __name__ == "__main__":
-    main()
--- pipeline/nyc_taxi_load.py
+++ pipeline/nyc_taxi_load.py
@@ -1,47 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+import click
 import pandas as pd
-
-
-# In[7]:
-
-
-# Read a sample of the data
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz')
-
-
-# In[8]:
-
-
-# Display first rows
-df.head()
-
-
-# In[9]:
-
-
-df.dtypes
-
-
-# In[10]:
-
-
-df.shape
-
-
-# In[11]:
-
-
-len(df)
-
-
-# In[12]:
-
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
 
 dtype = {
     "VendorID": "Int64",
@@ -67,85 +30,48 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-df = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates
-)
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-# In[13]:
+    engine = create_engine(f'postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
 
-df['tpep_pickup_datetime']
+    first = True
 
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            first = False
 
-# In[14]:
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
 
-
-get_ipython().system('uv add sqlalchemy psycopg2-binary')
-
-
-# In[15]:
-
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[16]:
-
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[17]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[23]:
-
-
-df_iter = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=100000
-)
-
-
-# In[19]:
-
-
-for df_chunk in df_iter:
-    print(len(df_chunk))
-
-
-# In[20]:
-
-
-get_ipython().system('uv add tqdm')
-
-
-# In[21]:
-
-
-from tqdm.auto import tqdm
-
-
-# In[24]:
-
-
-# Insert the data in chunks to PostgreSQL
-for df_chunk in tqdm(df_iter):
-    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    run()
